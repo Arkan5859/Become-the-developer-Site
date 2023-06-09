f728dcbd49ef2e4266bea1cff628b4f0c0155f41
Arkan5859 <przemekkoscielski477@gmail.com>
2023-06-09 02:07:41 +0200

    Formating

--- Typed.js
+++ Typed.js
@@ -1,5 +1,4 @@
-
-var typed = new Typed('#element', {
-  strings: ['Website', 'FrontEnd','BackEnd', 'FullStack'],
+var typed = new Typed("#element", {
+  strings: ["Website", "FrontEnd", "BackEnd", "FullStack"],
   typeSpeed: 75,
 });
